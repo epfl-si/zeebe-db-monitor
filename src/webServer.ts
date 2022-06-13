524750c0d06ba937f0fb87827f430ba586cd1dd1
Julien Delasoie <julien.delasoie@epfl.ch>
2022-06-13 10:12:37 +0200

    Add a rate limiter, as a too many db read could be catastrophic for the production

--- src/webServer.ts
+++ src/webServer.ts
@@ -1,8 +1,21 @@
 import {Request, Response} from "express";
 import { metricsRegistry } from "./metrics";
+import rateLimit from 'express-rate-limit'
 
 const express = require('express');
 export const expressApp = express();
+
+/**
+ * Yep, a rate limiter, as a too many db read could be catastrophic for the production
+ */
+const limiter = rateLimit({
+  windowMs: 2 * 1000, // 2 sec
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+expressApp.use(limiter)
 
 // Prometheus metrics route
 expressApp.get('/metrics', async (req: Request, res: Response) => {
