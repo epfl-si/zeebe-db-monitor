bbb9210e43521899a8511af7b59217086b7fde38
Julien Delasoie <julien.delasoie@epfl.ch>
2022-06-30 15:35:37 +0200

    Remove unused prometheus registry

--- src/metrics.ts
+++ src/metrics.ts
@@ -2,44 +2,36 @@ import {zdb} from "./zeebeDB";
 import {columnFamiliesNames} from "./zbColumnFamilies";
 
 const client = require("prom-client");
-export const metricsRegistry = new client.Registry();
-
 
 client.collectDefaultMetrics({
   app: 'zeebe-db-monitor',
   prefix: '',
   timeout: 10000,
   gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
-  register: metricsRegistry
 });
 
 // Create a histogram of the time to read the DB
-metricsRegistry.registerMetric(
-  new client.Histogram({
+export const zeebe_db_read_duration_seconds = new client.Histogram({
     name: 'zeebe_db_read_duration_seconds',
     help: 'Duration to count the entries for all the columnFamilies in ZeebeDB in seconds',
-  })
-);
+})
 
-// Register column families gauge
-metricsRegistry.registerMetric(
-  new client.Gauge({
-    name: `zeebe_db_column_family_entries`,
-    help: `Number of elements per column families inside the db`,
-    labelNames: ['db_name', 'column_family'],
-    async collect() {
-      // Set the mesure on all the column family
-      columnFamiliesNames.map(async (columnFamilyName) => {
-        let count: number|undefined;
-        await zdb.walkColumnFamily(columnFamilyName, function() {
-          !count ? count = 1 : count++;
-        })
-        if (count) {
-          this.set(
-            {db_name: 'runtime', column_family: columnFamilyName}, count
-          );
-        }
+new client.Gauge({
+  name: `zeebe_db_column_family_entries`,
+  help: `Number of elements per column families inside the db`,
+  labelNames: ['db_name', 'column_family'],
+  async collect() {
+    // Set the mesure on all the column family
+    columnFamiliesNames.map(async (columnFamilyName) => {
+      let count: number|undefined;
+      await zdb.walkColumnFamily(columnFamilyName, function() {
+        !count ? count = 1 : count++;
       })
-    }
-  })
-);
+      if (count) {
+        this.set(
+          {db_name: 'runtime', column_family: columnFamilyName}, count
+        );
+      }
+    })
+  }
+})
--- src/webServer.ts
+++ src/webServer.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
-import { metricsRegistry } from "./metrics";
+import { zeebe_db_read_duration_seconds} from "./metrics";
 import rateLimit from 'express-rate-limit'
+import {register} from "prom-client";
 
 const express = require('express');
 export const expressApp = express();
@@ -20,10 +21,10 @@ expressApp.use(limiter)
 // Prometheus metrics route
 expressApp.get('/metrics', async (req: Request, res: Response) => {
   // Start the HTTP request timer, saving a reference to the returned method
-  const end = metricsRegistry.getSingleMetric('zeebe_db_read_duration_seconds').startTimer();
+  const end = zeebe_db_read_duration_seconds.startTimer();
 
-  res.setHeader('Content-Type', metricsRegistry.contentType);
-  res.send(await metricsRegistry.metrics());
+  res.setHeader('Content-Type', register.contentType);
+  res.send(await register.metrics());
 
   // End timer and add labels
   end();
