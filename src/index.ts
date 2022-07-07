8a304624ea87dca7c67cd49bb314a3c943ee578d
Julien Delasoie <julien.delasoie@epfl.ch>
2022-07-07 11:22:42 +0200

    Remove unused closing node processes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import {expressApp} from "./webServer";
-import {zdb} from "./zeebeDB";
 
 require('dotenv').config()
 
 expressApp.listen(8080, () => console.log('Server metrics are currently exposed on /metrics...'));
-
-process.on( 'SIGINT', function() {
-  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
-  // some other closing procedures go here
-  zdb?.close()
-  process.exit( );
-})
