cfbbd3a4bd4449190b716551c7090a61368ca33f
Julien Delasoie <julien.delasoie@epfl.ch>
2025-12-22 15:19:02 +0100

    fix: Fix const being using before init

--- src/metrics/familiesKeyMetric.ts
+++ src/metrics/familiesKeyMetric.ts
@@ -1,8 +1,10 @@
 import {client} from "../promClient.js";
 
 import {singleFlightLdbOperations} from "./collector.js";
-import {bucketForLdbOperations} from "./index.js";
 
+export const bucketForLdbOperations = [
+  0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 5, 7
+]
 
 // Create a histogram of the time to read all entries DB
 export const zeebe_db_column_families_keys_read_duration_seconds = new client.Histogram({
--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -2,9 +2,7 @@ import {client} from "../promClient.js";
 import {zeebe_db_column_families_keys_read_duration_seconds, zeebeColumnFamiliesGauge} from "./familiesKeyMetric.js";
 //import {zeebe_db_incidents_per_message_read_duration_seconds, zeebeIncidentsPerMessageGauge} from "./incidentsMetric.js";
 
-export const bucketForLdbOperations = [
-  0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 5, 7
-]
+
 
 export const defaultMetricsRegistry = new client.Registry()
 export const zeebeMetricsRegistry = new client.Registry()
